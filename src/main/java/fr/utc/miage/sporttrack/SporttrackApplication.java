4946ab542c3fe75b2d800651f2c732b78d42ed26
jimmy <46294820+jimmy-txi@users.noreply.github.com>
2026-04-07 15:19:32 +0200

    Base Project Structure

---
+++ src/main/java/fr/utc/miage/sporttrack/SporttrackApplication.java
@@ -0,0 +1,13 @@
+package fr.utc.miage.sporttrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SporttrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SporttrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/utc/miage/sporttrack/SporttrackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.utc.miage.sporttrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SporttrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
