d1d70c4968d8b318a13474d434168b602a5b5912
jimmy <46294820+jimmy-txi@users.noreply.github.com>
2026-04-08 16:20:37 +0200

    Update baseController.java

--- src/main/java/fr/utc/miage/sporttrack/controller/baseController.java
+++ src/main/java/fr/utc/miage/sporttrack/controller/baseController.java
@@ -13,6 +13,7 @@ public class baseController {
     public String hello() {
         return "Hello, World!";
     }
+    // super simple endpoint to test if the server is running
 
     @GetMapping("/")
     public String home() {
