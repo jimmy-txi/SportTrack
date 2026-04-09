7f03636f31bb78c9a426a2dd8000a082e06064b1
Zhengyi Chen <chenzhengyifr@gmail.com>
2026-04-09 15:05:15 +0200

    feat: refuse Lombok

--- src/main/java/fr/utc/miage/sporttrack/entity/User/Athlete.java
+++ src/main/java/fr/utc/miage/sporttrack/entity/User/Athlete.java
@@ -3,13 +3,9 @@ package fr.utc.miage.sporttrack.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name = "athletes")
-@Setter
-@Getter
 public class Athlete extends User {
 
     @Column(name = "gender")
@@ -30,4 +26,55 @@ public class Athlete extends User {
     @Column(name = "bio", columnDefinition = "TEXT")
     private String bio;
 
-}
+    // --- Getters ---
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public String getPracticeLevel() {
+        return practiceLevel;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    // --- Setters ---
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public void setPracticeLevel(String practiceLevel) {
+        this.practiceLevel = practiceLevel;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
--- src/main/java/fr/utc/miage/sporttrack/entity/User/User.java
+++ src/main/java/fr/utc/miage/sporttrack/entity/User/User.java
@@ -5,12 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.Objects;
 
-@Getter
 @MappedSuperclass
 public abstract class User {
 
@@ -24,16 +21,50 @@ public abstract class User {
     @Column(nullable = false)
     private String password;
 
-    @Setter
     @Column(name = "last_name")
     private String lastName;
 
-    @Setter
     @Column(name = "first_name")
     private String firstName;
 
     @Column(nullable = false, unique = true)
     private String email;
+
+    // --- Getters ---
+
+    public Integer getIdU() {
+        return idU;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // --- Setters ---
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
     public void setUsername(String username) {
         if (isBlank(username)) {
@@ -67,16 +98,16 @@ public abstract class User {
         return Objects.hash(idU, email);
     }
 
-    //for verify the String value not blank
+    // for verify the String value not blank
     protected boolean isBlank(String value) {
         return value == null || value.trim().isEmpty();
     }
 
-    public void login(){
-        //logic
+    public void login() {
+        // logic
     }
 
-    public void logout(){
-        //logic
+    public void logout() {
+        // logic
     }
 }
