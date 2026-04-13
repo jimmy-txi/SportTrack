a96b2f088050993c33c8af6fc8957d87907f36d0
jimmy <46294820+jimmy-txi@users.noreply.github.com>
2026-04-13 14:11:23 +0200

    update

--- src/main/java/fr/utc/miage/sporttrack/SporttrackApplication.java
+++ src/main/java/fr/utc/miage/sporttrack/SporttrackApplication.java
@@ -1,29 +1,13 @@
 package fr.utc.miage.sporttrack;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SporttrackApplication {
 
-    public static void main(String[] args) {
-        try {
-            Path dbDir = Paths.get("/opt/sporttrack/data/db");
-            Files.createDirectories(dbDir);
+	public static void main(String[] args) {
+		SpringApplication.run(SporttrackApplication.class, args);
+	}
 
-            Path dbFile = dbDir.resolve("app.db");
-            if (Files.notExists(dbFile)) {
-                Files.createFile(dbFile);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Impossible de créer le dossier/fichier SQLite", e);
-        }
-
-        SpringApplication.run(SporttrackApplication.class, args);
-    }
 }
